ef9cdc3461823876901133f118ca7f6b003d48cb
Adil Hussain <adil.cs.work@gmail.com>
2021-02-11 22:23:57 +0530

    download generated

--- crawl.js
+++ crawl.js
@@ -2,34 +2,52 @@ const puppeteer = require('puppeteer');
 const http = require('https'); // or 'https' for https:// URLs
 const fs = require('fs');
 const download = require('download');
-
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+    path: 'noruto_shippuden.csv',
+    header: [
+        {id: 'name', title: 'NAME'},
+        {id: 'player', title: 'PLAYER URL'},
+        {id:'download',title:'DOWNLOAD URL'}
+    ]
+});
 const BASE_URL = "https://ww.kissanimes.tv";
-const INITIAL_URL = BASE_URL +"/watch/naruto-shippuuden-dub-episode-162";
+const INITIAL_URL = BASE_URL +"/watch/naruto-shippuuden-dub-episode-110";
 const DIR = '/home/adilh/Downloads/Video/NarutoShippuden/';
 (async () => {
  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   let nextUrl = INITIAL_URL;
-  for (i=0;i<317;i++) {
+  for (i=0;i<135;i++) {
   await page.goto(nextUrl);
-  let fileName = nextUrl.split('/').pop()+".mp4"
+  let fileName = nextUrl.split('/').pop()+".mp4";
+  let videoUrl='',downloadUrl='';
   // Get the "viewport" of the page, as reported by the page.
-  const videoUrl = await page.evaluate(() => {
-      const next =  "https://ww.kissanimes.tv"+$('.btn_next_and_prev')[0].childNodes[3].getAttribute('href')
+  try {
+    videoUrl = await page.evaluate(() => {
+      const next =  "https://ww.kissanimes.tv"+$('.btn_next_and_prev').children().last('a')[0].getAttribute('href')
     return [document.getElementById("player_container").childNodes[0].getAttribute("src"),next]
   });
   nextUrl = videoUrl[1];
   await page.goto(videoUrl[0]);
-  const downloadUrl=await page.evaluate(()=>{
+  downloadUrl=await page.evaluate(()=>{
     const delay = ms => new Promise(res => setTimeout(res, ms));
-    return delay(1000).then(()=>{
+    return delay(5).then(()=>{
         return jwplayer("myVideo").getConfig().sources[0].file
     }).catch((err)=>err);
   });
+} catch(err){
+  console.log(err);
+  continue;
+}
   try {
-    const data = await download(downloadUrl);
-    fs.writeFileSync(DIR+fileName,data);
-    console.log('downloaded '+fileName);
+    const records = [
+      {name: fileName,  player: videoUrl[0],download:downloadUrl}
+  ];
+    // const data = await download(downloadUrl);
+    // fs.writeFileSync(DIR+fileName,data);
+    await csvWriter.writeRecords(records) 
+    console.log('added '+fileName);
   } catch(err){
       console.log(err);
   }
